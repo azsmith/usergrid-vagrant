d4c48b8313c406d1c182331211655598110d5039
Andrew Smith <v-andsmi@microsoft.com>
2015-08-30 11:09:46 -0700

    updated portal stuffs

--- install_usergrid.sh
+++ install_usergrid.sh
@@ -18,16 +18,17 @@
 
 # purge old Node.js, add repo for new Node.js
 apt-get purge nodejs npm
+apt-get install software-properties-common
 apt-add-repository -y ppa:chris-lea/node.js
 
 # install what we need for building and running Usergrid Stack and Portal
 apt-get -y update
-apt-get -y install tomcat7 unzip git maven nodejs python-software-properties python g++ make
+apt-get -y install tomcat7 unzip git maven nodejs-legacy npm python-software-properties python g++ make
 /etc/init.d/tomcat7 stop
 
 # fetch usergrid code in our home dir
 cd /home/vagrant
-git clone https://git-wip-us.apache.org/repos/asf/incubator-usergrid.git usergrid
+git clone https://git-wip-us.apache.org/repos/asf/usergrid.git usergrid
 cd usergrid
 git checkout two-dot-o
 
@@ -43,13 +44,13 @@ mvn -DskipTests=true clean install
 cd rest/target
 rm -rf /var/lib/tomcat7/webapps/*
 cp -r ROOT.war /var/lib/tomcat7/webapps
-mkdir -p /usr/share/tomcat7/lib 
+mkdir -p /usr/share/tomcat7/lib
 
 # write Usergrid config
 cd /vagrant
-groovy config_usergrid.groovy > /usr/share/tomcat7/lib/usergrid-deployment.properties 
+groovy config_usergrid.groovy > /usr/share/tomcat7/lib/usergrid-deployment.properties
 
-# configure Tomcat memory and and hook up Log4j because Usergrid uses it 
+# configure Tomcat memory and and hook up Log4j because Usergrid uses it
 cd /home/vagrant
 cat >> /usr/share/tomcat7/bin/setenv.sh << EOF
 export JAVA_OPTS="-Xmx450m -Dlog4j.configuration=file:///usr/share/tomcat7/lib/log4j.properties -Dlog4j.debug=false"
@@ -59,11 +60,11 @@ cp usergrid/stack/rest/src/test/resources/log4j.properties /usr/share/tomcat7/li
 
 # build and deploy Usergrid Portal to Tomcat
 cd /home/vagrant/usergrid/portal
-./build.sh 
+./build.sh
 cd dist
 mkdir /var/lib/tomcat7/webapps/portal
 cp -r usergrid-portal/* /var/lib/tomcat7/webapps/portal
-sed -i.bak "s/https\:\/\/api.usergrid.com/http\:\/\/${PUBLIC_HOSTNAME}:8080/" /var/lib/tomcat7/webapps/portal/config.js 
+sed -i.bak "s/https\:\/\/api.usergrid.com/http\:\/\/${PUBLIC_HOSTNAME}:8080/" /var/lib/tomcat7/webapps/portal/config.js
 
 # go!
 /etc/init.d/tomcat7 restart
